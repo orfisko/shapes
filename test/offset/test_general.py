574863d8a8cfc24ce1ce00796ea9b14770fe5956
Maxim Shtuchka <maxim1000@gmail.com>
2023-09-04 10:37:44 +0300

    added tests to ensure that the input is not changed and the output has no links to the input

--- test/offset/test_general.py
+++ test/offset/test_general.py
@@ -36,3 +36,20 @@ def test_that_topology_is_the_same(polyhedron_cutout_sloped):
                     same_in_input = (input_face1.vertices[vertex1_index] is input_face2.vertices[vertex2_index])
                     same_in_output = (output_face1.vertices[vertex1_index] is output_face2.vertices[vertex2_index])
                     assert same_in_output == same_in_input, f"Vertex #{vertex1_index} of face #{face1_index} is the same as vertex #{vertex2_index} of face #{face2_index} in the input but not in the output"
+
+def test_that_the_input_has_not_been_changed(polyhedron_cutout_sloped):
+    input = polyhedron_cutout_sloped()
+    before = [[[vertex.x, vertex.y, vertex.z] for vertex in face.vertices] for face in input.faces]
+    output = apply_offset(polyhedron=input, offset=Decimal(10))
+    after = [[[vertex.x, vertex.y, vertex.z] for vertex in face.vertices] for face in input.faces]
+    assert after == before, f"The operation changed coordinates of the input object"
+
+def test_that_input_and_output_do_not_reference_same_objects(polyhedron_cutout_sloped):
+    input = polyhedron_cutout_sloped()
+    output = apply_offset(polyhedron=input, offset=Decimal(10))
+    for face1 in input.faces:
+        for face2 in output.faces:
+            assert face1 is not face2, "The output shares a face object with the input"
+            for vertex1 in face1.vertices:
+                for vertex2 in face2.vertices:
+                    assert vertex1 is not vertex2, "The output shares a vertex object with the input"
