1f486f089ae45db1fe8b4815f57c44b026f117b9
Maxim Shtuchka <maxim1000@gmail.com>
2023-09-06 22:54:01 +0300

    panel_shapes: added prioritizer that decides which panels get more material

--- source/panel_shapes.py
+++ source/panel_shapes.py
@@ -7,8 +7,8 @@ from source.geometry import *
 
 
 def make_plate(
-    outer_face_contour: [Vertex],
-    inner_face_contour: [Vertex]
+    outer_face_contour: List[Vertex],
+    inner_face_contour: List[Vertex]
 ) -> Polyhedron:
     if len(outer_face_contour)!=len(inner_face_contour):
         raise ValueError("make_plate: outer and inner contours have different number of vertices")
@@ -33,6 +33,7 @@ def make_vertex(position: Vector3d)->Vertex:
 def generate_panel_shapes(
     outer_polyhedron: Polyhedron,
     inner_polyhedron: Polyhedron,
+    prioritizer
 ) -> [Polyhedron]:
     """
     Generates panel shapes using inner and outer polyhedrons. They should have
@@ -50,21 +51,22 @@ def generate_panel_shapes(
         inner=inner_polyhedron.faces[face_index].vertices[:]
         for vertex_index in range(len(outer)):
             adjacent_face_indices=vertex_id_to_face_indices[id(outer[vertex_index])]
-            adjacent_face_indices.sort()
-            main_face_inner_vertex=make_vertex(compute_three_planes_intersection(
-                compute_face_plane(inner_polyhedron.faces[adjacent_face_indices[0]]),
-                compute_face_plane(outer_polyhedron.faces[adjacent_face_indices[1]]),
-                compute_face_plane(outer_polyhedron.faces[adjacent_face_indices[2]])))
-            secondary_face_inner_vertex=make_vertex(compute_three_planes_intersection(
-                compute_face_plane(inner_polyhedron.faces[adjacent_face_indices[0]]),
-                compute_face_plane(inner_polyhedron.faces[adjacent_face_indices[1]]),
-                compute_face_plane(outer_polyhedron.faces[adjacent_face_indices[2]])))
-            if face_index==adjacent_face_indices[0]:
-                inner[vertex_index]=main_face_inner_vertex
-            elif face_index==adjacent_face_indices[1]:
-                outer[vertex_index]=main_face_inner_vertex
-                inner[vertex_index]=secondary_face_inner_vertex
-            else:
-                outer[vertex_index]=secondary_face_inner_vertex
+            priorities=prioritizer(adjacent_face_indices)
+            current_priority=priorities[adjacent_face_indices.index(face_index)]
+            #inner
+            planes=[]
+            for c in range(3):
+                surface=outer_polyhedron if priorities[c]>current_priority else inner_polyhedron
+                planes.append(compute_face_plane(surface.faces[adjacent_face_indices[c]]))
+            inner[vertex_index]=make_vertex(
+                compute_three_planes_intersection(planes[0],planes[1],planes[2]))
+            #outer
+            planes=[]
+            for c in range(3):
+                surface=inner_polyhedron if priorities[c]<current_priority else outer_polyhedron
+                planes.append(compute_face_plane(surface.faces[adjacent_face_indices[c]]))
+            outer[vertex_index]=make_vertex(
+                compute_three_planes_intersection(planes[0],planes[1],planes[2]))
+            print(outer[vertex_index])
         panels.append(make_plate(outer,inner))
     return panels
