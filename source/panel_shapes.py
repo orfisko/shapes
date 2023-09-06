9e07296545e61ebf1fb135c5923589fd951a404f
Maxim Shtuchka <maxim1000@gmail.com>
2023-09-06 12:34:15 +0300

    implemented prioritization of edges/corners between faces (for now based on index)

--- source/panel_shapes.py
+++ source/panel_shapes.py
@@ -6,6 +6,30 @@ from source.model import Polyhedron, Vertex, Face
 from source.geometry import *
 
 
+def make_plate(
+    outer_face_contour: [Vertex],
+    inner_face_contour: [Vertex]
+) -> Polyhedron:
+    if len(outer_face_contour)!=len(inner_face_contour):
+        raise ValueError("make_plate: outer and inner contours have different number of vertices")
+    result=Polyhedron(faces=[])
+    result.faces.append(Face(vertices=outer_face_contour))
+    for edge_index in range(len(outer_face_contour)):
+        next_index=(edge_index+1)%len(outer_face_contour)
+        result.faces.append(Face(vertices=[
+            outer_face_contour[next_index],
+            outer_face_contour[edge_index],
+            inner_face_contour[edge_index],
+            inner_face_contour[next_index]]))
+    result.faces.append(Face(vertices=inner_face_contour[::-1]))
+    return result
+def compute_face_plane(face: Face):
+    return Plane3d(
+        origin=face.vertices[0].vector(),
+        normal=calculate_contour_normal([v.vector() for v in face.vertices]),
+    )
+def make_vertex(position: Vector3d)->Vertex:
+    return Vertex(position.x,position.y,position.z)
 def generate_panel_shapes(
     outer_polyhedron: Polyhedron,
     inner_polyhedron: Polyhedron,
@@ -16,19 +40,31 @@ def generate_panel_shapes(
     Returns a list of generated panels in the same order as faces in the
     polyhedrons.
     """
+    vertex_id_to_face_indices:DefaultDict[int,List[int]]=defaultdict(list)
+    for face_index in range(len(outer_polyhedron.faces)):
+        for vertex in outer_polyhedron.faces[face_index].vertices:
+            vertex_id_to_face_indices[id(vertex)].append(face_index)
     panels=[]
     for face_index in range(len(inner_polyhedron.faces)):
-        outer_face=outer_polyhedron.faces[face_index]
-        inner_face=inner_polyhedron.faces[face_index]
-        panel=Polyhedron(faces=[])
-        panel.faces.append(outer_face)
-        for edge_index in range(len(outer_face.vertices)):
-            next_index=(edge_index+1)%len(outer_face.vertices)
-            panel.faces.append(Face(vertices=[
-                outer_face.vertices[next_index],
-                outer_face.vertices[edge_index],
-                inner_face.vertices[edge_index],
-                inner_face.vertices[next_index]]))
-        panel.faces.append(Face(vertices=inner_face.vertices[::-1]))
-        panels.append(panel)
+        outer=outer_polyhedron.faces[face_index].vertices[:]
+        inner=inner_polyhedron.faces[face_index].vertices[:]
+        for vertex_index in range(len(outer)):
+            adjacent_face_indices=vertex_id_to_face_indices[id(outer[vertex_index])]
+            adjacent_face_indices.sort()
+            main_face_inner_vertex=make_vertex(compute_three_planes_intersection(
+                compute_face_plane(inner_polyhedron.faces[adjacent_face_indices[0]]),
+                compute_face_plane(outer_polyhedron.faces[adjacent_face_indices[1]]),
+                compute_face_plane(outer_polyhedron.faces[adjacent_face_indices[2]])))
+            secondary_face_inner_vertex=make_vertex(compute_three_planes_intersection(
+                compute_face_plane(inner_polyhedron.faces[adjacent_face_indices[0]]),
+                compute_face_plane(inner_polyhedron.faces[adjacent_face_indices[1]]),
+                compute_face_plane(outer_polyhedron.faces[adjacent_face_indices[2]])))
+            if face_index==adjacent_face_indices[0]:
+                inner[vertex_index]=main_face_inner_vertex
+            elif face_index==adjacent_face_indices[1]:
+                outer[vertex_index]=main_face_inner_vertex
+                inner[vertex_index]=secondary_face_inner_vertex
+            else:
+                outer[vertex_index]=secondary_face_inner_vertex
+        panels.append(make_plate(outer,inner))
     return panels
