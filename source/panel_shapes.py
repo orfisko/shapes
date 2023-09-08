a53bc31806556df702a0f54caeaa0b40e546f06d
hendrik <hendrik@dekastenman.be>
2023-09-08 15:57:52 +0200

    kwargs added to generate panel shape, test case added with partial offset

--- source/panel_shapes.py
+++ source/panel_shapes.py
@@ -1,67 +1,89 @@
-from _decimal import Decimal
 from collections import defaultdict
-from typing import Optional, DefaultDict, List
+from typing import DefaultDict, List
 
-from source.model import Polyhedron, Vertex, Face
 from source.geometry import *
+from source.model import Vertex, Face, Polyhedron
 
 
 def make_plate(
-    outer_face_contour: List[Vertex],
-    inner_face_contour: List[Vertex]
+    outer_face_contour: List[Vertex], inner_face_contour: List[Vertex]
 ) -> Polyhedron:
-    if len(outer_face_contour)!=len(inner_face_contour):
-        raise ValueError("make_plate: outer and inner contours have different number of vertices")
-    result=Polyhedron(faces=[])
+    if len(outer_face_contour) != len(inner_face_contour):
+        raise ValueError(
+            "make_plate: outer and inner contours have different number of vertices"
+        )
+    result = Polyhedron(faces=[])
     result.faces.append(Face(vertices=outer_face_contour))
     for edge_index in range(len(outer_face_contour)):
-        next_index=(edge_index+1)%len(outer_face_contour)
-        result.faces.append(Face(vertices=[
-            outer_face_contour[next_index],
-            outer_face_contour[edge_index],
-            inner_face_contour[edge_index],
-            inner_face_contour[next_index]]))
+        next_index = (edge_index + 1) % len(outer_face_contour)
+        result.faces.append(
+            Face(
+                vertices=[
+                    outer_face_contour[next_index],
+                    outer_face_contour[edge_index],
+                    inner_face_contour[edge_index],
+                    inner_face_contour[next_index],
+                ]
+            )
+        )
     result.faces.append(Face(vertices=inner_face_contour[::-1]))
     return result
-def make_vertex(position: Vector3d)->Vertex:
-    return Vertex(position.x,position.y,position.z)
+
+
+def make_vertex(position: Vector3d) -> Vertex:
+    return Vertex(position.x, position.y, position.z)
+
+
 def generate_panel_shapes(
-    outer_polyhedron: Polyhedron,
-    inner_polyhedron: Polyhedron,
-    prioritizer
+    outer_polyhedron: Polyhedron, inner_polyhedron: Polyhedron, prioritizer, **kwargs
 ) -> [Polyhedron]:
     """
     Generates panel shapes using inner and outer polyhedrons. They should have
     faces/vertices in the same order and of the same number.
-    Returns a list of generated panels in the same order as faces in the
-    polyhedrons.
+    Args:
+        outer_polyhedron:
+        inner_polyhedron:
+        prioritizer: priotization function to decide which panel takes precedence over the other.
+        **kwargs: contextual information being passed to the prioritizer function
+    Returns:
+         a list of generated panels in the same order as faces in the polyhedrons.
     """
-    vertex_id_to_face_indices:DefaultDict[int,List[int]]=defaultdict(list)
+    vertex_id_to_face_indices: DefaultDict[int, List[int]] = defaultdict(list)
     for face_index in range(len(outer_polyhedron.faces)):
         for vertex in outer_polyhedron.faces[face_index].vertices:
             vertex_id_to_face_indices[id(vertex)].append(face_index)
-    panels=[]
+    panels = []
     for face_index in range(len(inner_polyhedron.faces)):
-        outer=outer_polyhedron.faces[face_index].vertices[:]
-        inner=inner_polyhedron.faces[face_index].vertices[:]
+        outer = outer_polyhedron.faces[face_index].vertices[:]
+        inner = inner_polyhedron.faces[face_index].vertices[:]
         for vertex_index in range(len(outer)):
-            adjacent_face_indices=vertex_id_to_face_indices[id(outer[vertex_index])]
-            priorities=prioritizer(adjacent_face_indices)
-            current_priority=priorities[adjacent_face_indices.index(face_index)]
-            #inner
-            planes=[]
+            adjacent_face_indices = vertex_id_to_face_indices[id(outer[vertex_index])]
+            priorities = prioritizer(outer_polyhedron, adjacent_face_indices, **kwargs)
+            current_priority = priorities[adjacent_face_indices.index(face_index)]
+            # inner
+            planes = []
             for c in range(3):
-                surface=outer_polyhedron if priorities[c]>current_priority else inner_polyhedron
+                surface = (
+                    outer_polyhedron
+                    if priorities[c] > current_priority
+                    else inner_polyhedron
+                )
                 planes.append(surface.faces[adjacent_face_indices[c]].compute_plane())
-            inner[vertex_index]=make_vertex(
-                compute_three_planes_intersection(planes[0],planes[1],planes[2]))
-            #outer
-            planes=[]
+            inner[vertex_index] = make_vertex(
+                compute_three_planes_intersection(planes[0], planes[1], planes[2])
+            )
+            # outer
+            planes = []
             for c in range(3):
-                surface=inner_polyhedron if priorities[c]<current_priority else outer_polyhedron
+                surface = (
+                    inner_polyhedron
+                    if priorities[c] < current_priority
+                    else outer_polyhedron
+                )
                 planes.append(surface.faces[adjacent_face_indices[c]].compute_plane())
-            outer[vertex_index]=make_vertex(
-                compute_three_planes_intersection(planes[0],planes[1],planes[2]))
+            outer[vertex_index] = make_vertex(
+                compute_three_planes_intersection(planes[0], planes[1], planes[2])
+            )
             print(outer[vertex_index])
-        panels.append(make_plate(outer,inner))
+        panels.append(make_plate(outer, inner))
     return panels
