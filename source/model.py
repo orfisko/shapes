b51c6557bdc3aebba49afa28cf4e301913922849
Maxim Shtuchka <maxim1000@gmail.com>
2023-09-10 19:13:25 +0300

    removed property caching because it interfered with equality checks

--- source/model.py
+++ source/model.py
@@ -24,16 +24,13 @@ class Vertex:
     y: Decimal
     z: Decimal
     normal: Optional[Normal] = None  # This can allow to cache the normal
-    _cached_vector: Vector3d = None
 
     def __add__(self, other):
         return Vertex(x=self.x + other.x, y=self.y + other.y, z=self.z + other.z)
 
     @property
     def vector(self) -> Vector3d:
-        if not self._cached_vector:
-            self._cached_vector = Vector3d(float(self.x), float(self.y), float(self.z))
-        return self._cached_vector
+        return Vector3d(float(self.x), float(self.y), float(self.z))
 
 
 @dataclass(**default_config)
@@ -46,18 +43,15 @@ class Normal:
 @dataclass(**default_config)
 class Face:
     vertices: List[Vertex]
-    _cached_plane: Plane3d = None
 
     @property
     def plane(self):
-        if not self._cached_plane:
-            from source.general import calculate_contour_normal
+        from source.general import calculate_contour_normal
 
-            return Plane3d(
-                origin=self.vertices[0].vector,
-                normal=calculate_contour_normal([v.vector for v in self.vertices]),
-            )
-        return self._cached_plane
+        return Plane3d(
+            origin=self.vertices[0].vector,
+            normal=calculate_contour_normal([v.vector for v in self.vertices]),
+        )
 
     @property
     def orientation(self) -> Orientation:
