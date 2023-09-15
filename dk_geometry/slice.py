944a680ae64e7f4306a7ff25da2b32af25961621
Maxim Shtuchka <maxim1000@gmail.com>
2023-09-15 16:09:21 +0300

    implemented apply_slice_interval

--- dk_geometry/slice.py
+++ dk_geometry/slice.py
@@ -1,7 +1,8 @@
 from _decimal import Decimal
 from typing import List, Set
 
-from dk_geometry.model import Polyhedron, SliceInterval, Slice, Vector3d
+from dk_geometry.general import cut_polyhedron_by_plane
+from dk_geometry.model import Plane3d, Polyhedron, SliceInterval, Slice, Vector3d
 
 
 def apply_slice(polyhedron: Polyhedron, slice: Slice) -> List[Vector3d]:
@@ -26,7 +27,38 @@ def apply_slice_interval(polyhedron: Polyhedron, slice: SliceInterval) -> Polyhe
     Returns:
         a new polyhedron with the slice applied.
     """
-    return polyhedron
+    result = polyhedron
+    if slice.x0 is not None:
+        result = cut_polyhedron_by_plane(
+            result,
+            Plane3d(origin=Vector3d(slice.x0,0,0), normal=Vector3d(-1,0,0))
+        )
+    if slice.x1 is not None:
+        result = cut_polyhedron_by_plane(
+            result,
+            Plane3d(origin=Vector3d(slice.x1,0,0), normal=Vector3d(1,0,0))
+        )
+    if slice.y0 is not None:
+        result = cut_polyhedron_by_plane(
+            result,
+            Plane3d(origin=Vector3d(0,slice.y0,0), normal=Vector3d(0,-1,0))
+        )
+    if slice.y1 is not None:
+        result = cut_polyhedron_by_plane(
+            result,
+            Plane3d(origin=Vector3d(0,slice.y1,0), normal=Vector3d(0,1,0))
+        )
+    if slice.z0 is not None:
+        result = cut_polyhedron_by_plane(
+            result,
+            Plane3d(origin=Vector3d(0,0,slice.z0), normal=Vector3d(0,0,-1))
+        )
+    if slice.z1 is not None:
+        result = cut_polyhedron_by_plane(
+            result,
+            Plane3d(origin=Vector3d(0,0,slice.z1), normal=Vector3d(0,0,1))
+        )
+    return result
 
 
 def apply_slice_intervals(
